0b8604b5ab0068908f148554e11981e612f5de13
Choyoonyoung98 <yoon980208@gmail.com>
2022-05-28 21:27:41 +0900

    [UPDATE] Setting up different profiles 내용 업로드

--- course-catalog-service/src/main/kotlin/com/kotlinspring/service/GreetingService.kt
+++ course-catalog-service/src/main/kotlin/com/kotlinspring/service/GreetingService.kt
@@ -1,10 +1,14 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
 
 @Service
 class GreetingService {
-    fun retrieveGreeting(name: String) = "Hello $name"
+
+    @Value("\${message}")
+    lateinit var message: String //swift의 lazy와 같은 역할?
+    fun retrieveGreeting(name: String) = "$name, $message"
 
 }
