ea8782800cb54d7dab9f682f0362e82f66704049
Choyoonyoung98 <yoon980208@gmail.com>
2022-06-25 21:39:45 +0900

    [CREATE] tileLog demo 프로젝트 초기 셋팅 완료
    - mysql 연동 및 프로젝트 구조 셋팅

---
+++ demo-TileLog/src/main/kotlin/com/tilelog/DemoTileLogApplication.kt
@@ -0,0 +1,11 @@
+package com.tilelog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoTileLogApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoTileLogApplication>(*args)
+}
---
+++ demo-TileLog/src/test/kotlin/com/tilelog/DemoTileLogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tilelog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoTileLogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
